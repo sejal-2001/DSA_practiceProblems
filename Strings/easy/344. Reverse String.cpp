286b1954c844a74a983abcbafac9800af08ee93d
Sejal Kushwaha <67407760+sejal-2001@users.noreply.github.com>
2022-07-13 23:01:19 +0530

    Add files via upload

---
+++ Strings/easy/344. Reverse String.cpp
@@ -0,0 +1,22 @@
+que - Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int start =0;
+        int end = s.size()-1;
+        
+        while(start < end)
+        {
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+        
+    }
+};
+ 
