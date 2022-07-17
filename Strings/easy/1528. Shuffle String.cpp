c5fc0a5739ec36628d12e7b378f6e2abbbe7cdef
Sejal Kushwaha <67407760+sejal-2001@users.noreply.github.com>
2022-07-17 22:23:40 +0530

    Add files via upload

---
+++ Strings/easy/1528. Shuffle String.cpp
@@ -0,0 +1,13 @@
+que - You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+         string res = s;
+        for(int i = 0; i < indices.size(); i++)
+            res[indices[i]] = s[i];
+        return res;
+    }
+};
